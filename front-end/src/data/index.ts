8084ba16529ec1097166b838191a1626c5457a6b
kelderpassos <kelder.passos@gmail.com>
2023-04-17 18:35:01 -0300

    Feat: page user implemented with basic logic

---
+++ front-end/src/data/index.ts
@@ -0,0 +1,33 @@
+export const clientsDatabase: Record<string, string>[] = [
+  { name: 'Patrícia Magi',
+    cpf: '11111111111',
+    email: 'patricia-magi@ultracar.com.br',
+    car: 'Ford Fusion 2023',
+    service: 'troca de óleo'
+  },
+  { name: 'Jonathan Cristian',
+    cpf: '22222222222',
+    email: 'jonathan-cristian@ultracar.com.br',
+    car: 'Toyota Corolla 2023',
+    service: 'alinhamento'
+  },
+  { name: 'Genesis Pereira',
+    cpf: '33333333333',
+    email: 'genesis-pereira@ultracar.com.br',
+    car: 'Ford Fusion 2023',
+    service: 'revisão'
+  },
+  { name: 'Wilson Sales',
+    cpf: '44444444444',
+    email: 'wilson-sales@ultracar.com.br',
+    car: 'Ford Fusion 2023',
+    service: 'troca pastilha de freio'
+  },
+]
+
+export const employees = [
+  {id: 1, name: 'Joana'},
+  {id: 2, name: 'Joaquim'},
+  {id: 3, name: 'Afonso'},
+  {id: 4, name: 'Larissa'},
+]
--- front-end/src/pages/users/[user].tsx
+++ front-end/src/pages/users/[user].tsx
@@ -1,7 +1,69 @@
+import { ChangeEventHandler, useState } from 'react'
+import QRCode from 'react-qr-code'
+import { clientsDatabase, employees } from '../../data'
+import { useRouter } from 'next/router'
+
+const initialState = {
+	employee: 'Joana',
+  parts: ''
+}
+
 export default function User() {
+  const [input, setInput] = useState<Record<string, string>>(initialState)
+  const [isChecked, setIsChecked] = useState<boolean>(false)
+  const router = useRouter();
+
+  const user = clientsDatabase[0]
+
+  const handleInput: React.ChangeEventHandler<HTMLInputElement> = ({ target: {id, value} }) => {
+    setInput((prevState) => ({...prevState, [id]: value}))   
+  }
+
+  const handleSelectInput: React.ChangeEventHandler<HTMLSelectElement> = ({ target: {id, value} }) => {
+    setInput((prevState) => ({...prevState, [id]: value}))
+  }
+
+  const handleCheckbox: ChangeEventHandler<HTMLInputElement> = ({ target: { checked } }) => {
+		setIsChecked(checked);
+	}
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault()
+    router.push('/services')
+  }
+
   return (
     <div>
-      teste
+      <section>
+        <h1>Visualizar dados do cliente</h1>
+        <QRCode value={`
+          ${user.name},
+          ${user.cpf},
+          ${user.email},
+          ${user.car},
+          ${user.service}`
+        }/>
+      </section>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="employee">Colaborador designado
+          <select id='employee' onChange={handleSelectInput}>{
+            employees.map(({id, name}) => (
+              <option key={id} value={name}>{name}</option>              
+            ))}
+          </select>
+        </label>
+        <label htmlFor="parts">Necessita de peças?
+          <input
+            type="checkbox"
+            id="parts"
+            checked={isChecked}
+            onChange={handleCheckbox}
+          />
+        </label>
+        {isChecked ? (<input id='parts' type='text' value={input.parts} onChange={handleInput}/>) : ''}
+        <button type='submit'>Fechar serviço</button>
+      </form>
     </div>
   )
 }
