cf0075698536996892ed292d11b3239b06fcd3c6
kelderpassos <kelder.passos@gmail.com>
2023-04-17 14:24:54 -0300

    Feat: create a mock database of users

---
+++ front-end/src/data/index.tsx
@@ -0,0 +1,6 @@
+export const clientsDatabase = [
+  { name: 'Patrícia Magi', cpf: '11111111111', email: 'patricia-magi@ultracar.com.br'},
+  { name: 'Jonathan Cristian', cpf: '22222222222', email: 'jonathan-cristian@ultracar.com.br'},
+  { name: 'Genesis Pereira', cpf: '33333333333', email: 'genesis-pereira@ultracar.com.br'},
+  { name: 'Wilson Sales', cpf: '44444444444', email: 'wilson-sales@ultracar.com.br'},
+]
