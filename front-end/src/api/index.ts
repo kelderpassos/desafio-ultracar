701478b44adcde5d4f2d4175d99a210dc19d7ad1
kelderpassos <kelder.passos@gmail.com>
2023-04-18 14:56:31 -0300

    Feat: new function to fetch employees from mocked api

--- front-end/src/api/index.ts
+++ front-end/src/api/index.ts
@@ -1,5 +1,5 @@
-import { Client } from "@/interfaces/client"
-
+import { Client } from "@/interfaces"
+import { Employee } from "@/interfaces/employees"
 
 export const fetchByCpf = async (cpf: string) => {
   try {
@@ -7,6 +7,18 @@ export const fetchByCpf = async (cpf: string) => {
     const response = await request.json()
     
     return response[0] as Client
+    
+  } catch (error: any) {
+    throw new Error(error)
+  }
+}
+
+export const fetchEmployees = async () => {
+  try {
+    const request = await fetch(`http://localhost:3000/employees/`)
+    const response = await request.json()
+    
+    return response as Employee[]
     
   } catch (error: any) {
     throw new Error(error)
